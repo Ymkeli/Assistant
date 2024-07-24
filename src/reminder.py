199d6e8c16a679bec02c1e0ca6a638c072b30776
Ymke Wegereef <Ymkewe@gmail.com>
2024-07-24 15:09:24 +0200

    Check if the openAI response is a datetime object.

--- src/reminder.py
+++ src/reminder.py
@@ -1,6 +1,7 @@
 from open_ai import query_datetime
 from datetime import datetime
 import threading
+import re
 
 def remind(label):
     print("Reminder: " + label)
@@ -10,6 +11,10 @@ def set_reminder_at(target_time, label):
     time_diff = (target_time - now).total_seconds()
     timer = threading.Timer(time_diff, lambda: remind(label))
     timer.start()
+    
+def is_datetime_stamp(string):
+    match = re.search("\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}:\\d{2}", string)
+    return match != None
 
 def set_reminder(query):
     # Sets a reminder at the time provided in query
@@ -22,9 +27,13 @@ def set_reminder(query):
     
     response = query_datetime(query)
     
-    # Translate the reponse as a date-time object
-    format = "%d-%m-%Y %H:%M:%S"
-    datetime_object = datetime.strptime(response, format)
-    
-    set_reminder_at(datetime_object, reminder_label)   
-    return f"A reminder has been set at {response}."
+    # First, check if the response is a datetime object.
+    # If so, set a reminder. Else, ask for a new query.
+    if is_datetime_stamp(response):       
+        # Translate the reponse to a date-time object
+        format = "%d-%m-%Y %H:%M:%S"
+        datetime_object = datetime.strptime(response, format)
+        set_reminder_at(datetime_object, reminder_label)
+        return f"A reminder has been set at {response}."
+    else: 
+        return f"I could not set a reminder. Can you reformulate your query?"
